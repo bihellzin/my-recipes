d98b47583b958d6c80b0a98ce58c92d69ff7dff0
bihellzin <gof2@cin.ufpe.br>
2020-08-25 11:47:58 -0300

    recipe creation tests working and almost complete

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -11,10 +11,42 @@ class RecipesTest < ActionDispatch::IntegrationTest
     get recipes_url
     assert_response :success
   end
-  
-  test 'should get recipes listing' do
+
+  test 'should get recipes list' do
     get recipes_path
     assert_template 'recipes/index'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: @recipe.name
+  end
+
+  test 'should get recipe show' do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @user.chefname, response.body
+  end
+
+  test 'create a new valid recipe' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    recipe_name = 'fried butter'
+    recipe_description = 'very tasty american food'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: recipe_name, description: recipe_description } }
+    end
+    follow_redirect!
+    assert_match recipe_name.capitalize, response.body
+    assert_match recipe_description, response.body
+  end
+
+  test 'reject invalid recipe submission' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: ' ', description: ' ' } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 end
