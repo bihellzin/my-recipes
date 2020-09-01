4d9c1a3d617b602b2379270d040d5af0b1c4d162
bihellzin <gof2@cin.ufpe.br>
2020-09-01 16:45:53 -0300

    comments added without tests

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  before_action :require_user
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.build(comment_params)
+    @comment.chef = current_chef
+    if @comment.save
+      flash[:success] = 'Comment was created successfully'
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:danger] = 'Comment was not created'
+      redirect_back fallback_location: recipe_path(@recipe)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description)
+  end
+end
